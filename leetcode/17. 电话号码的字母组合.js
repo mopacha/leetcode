35eb91e732ce22554953bd14cef0a8011948fd20
王朋 <wangpeng@sensorsdata.cn>
2021-04-27 15:50:51 +0800

    add

---
+++ leetcode/17. 电话号码的字母组合.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function (digits) {
+    let somes = {
+        2: 'abc',
+        3: 'def',
+        4: 'ghi',
+        5: 'jkl',
+        6: 'mno',
+        7: 'pqr',
+        8: 'tuv',
+        9: 'wxyz'
+    }
+
+   
+
+};
